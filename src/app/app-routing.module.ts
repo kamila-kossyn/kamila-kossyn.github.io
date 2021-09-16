e962e21ca22a14c87778a72e51090e4b1540c8b6
Łukasz Narloch <lukassz111@gmail.com>
2021-09-16 14:39:14 +0200

    wip welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './page/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '',component: WelcomeComponent},
+  {path: 'welcome',component: WelcomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      initialNavigation: "enabled",
+      useHash: true,
+      onSameUrlNavigation: 'reload',
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
